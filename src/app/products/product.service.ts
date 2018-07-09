f7b91ba31b0dedb8114b2037215f8b75d8b267be
Jason Dailey <jdailey@hchb.com>
2018-07-09 14:54:29 -0400

    Through Section 10

---
+++ src/app/products/product.service.ts
@@ -0,0 +1,26 @@
+import { IProduct } from './product';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
+
+@Injectable()
+export class ProductService {
+  private productUrl = './api/products/products.json';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(this.productUrl)
+      .do(data => console.log('All: ' + JSON.stringify(data)))
+      .catch(this.handleError);
+  }
+
+  handleError(err: HttpErrorResponse) {
+    console.log(err.message);
+    return Observable.throw(err.message);
+  }
+}
